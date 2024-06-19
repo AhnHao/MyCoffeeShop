cfc9b60ca710ad5cbfdc89a4d1d65521cd14b6bb
AhnHao <haotrananh2@gmail.com>
2024-06-19 18:43:50 +0700

    add product page

--- app.js
+++ app.js
@@ -3,16 +3,29 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const connectDB = require('./config/database')
 
+const User = require('./models/user')
+
 const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
+const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use((req, res, next) => {
+  User.findById('66724e4409681dc6b818765b')
+    .then(user => {
+      req.user = user
+      next()
+    })
+    .catch(err => console.log(err))
+})
+
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 connectDB()
---
+++ controllers/admin.js
@@ -0,0 +1,33 @@
+const Product = require('../models/product')
+
+exports.getAddProduct = (req, res) => {
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing: false
+  })
+}
+
+exports.postAddProduct = (req, res) => {
+  const title = req.body.title
+  const imageUrl = req.body.imageUrl
+  const price = req.body.price
+  const description = req.body.description
+  const product = new Product({
+    title: title,
+    price: price,
+    description: description,
+    imageUrl: imageUrl,
+    userId: req.user
+  })
+  product
+    .save()
+    .then(result => {
+        console.log(req.body)
+        console.log(result)
+      console.log('Created Product')
+      res.redirect('/')
+    })
+    .catch(err => {
+        console.log(err)})
+}
