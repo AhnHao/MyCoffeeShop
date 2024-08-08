88de988bb7e12acda3493725d0ff1993d84c835c
AhnHao <haotrananh2@gmail.com>
2024-08-08 19:19:15 +0700

    new update error ui

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 app.use(authRoutes)
 
-app.use('/500', errorController.get505)
+app.use('/500', errorController.get500)
 app.use(errorController.get404)
 
 app.use((error, req, res, next) => {
--- controllers/error.js
+++ controllers/error.js
@@ -1,13 +1,11 @@
 exports.get404 = (req, res) => {
-  res
-    .status(404)
-    .render('404', {
-      pageTitle: 'Page Not Found',
-      path: '/404'
-    })
+  res.status(404).render('404', {
+    pageTitle: 'Page Not Found',
+    path: '/404'
+  })
 }
 
-exports.get505 = (req, res) => {
+exports.get500 = (req, res) => {
   res.status(500).render('500', {
     pageTitle: 'Error',
     path: '/500',
